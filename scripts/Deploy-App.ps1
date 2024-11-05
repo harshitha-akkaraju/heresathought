e590bf7c18d67cadcf2a4d9474eb866ed5c9915b
Harshitha Akkaraju <hakkaraj@microsoft.com>
2024-11-05 10:14:37 -0800

    web app server

---
+++ scripts/Deploy-App.ps1
@@ -0,0 +1,58 @@
+$repoRoot = ""
+
+function Write-Message {
+    param (
+        [string]$Message,
+        [string]$Color = "White",
+        [switch]$Stop
+    )
+
+    Write-Host -Object $Message -ForegroundColor $Color
+
+    if ($Stop) {
+        exit 1
+    }
+}
+
+if (-Not $repoRoot) {
+    Write-Message -Message "Variable `$repoRoot is not set. Please set the repo root path." -Color "Red" -Stop
+}
+
+$webAppBuild = "$repoRoot/app/build/web"
+$webAppBuildTmp = "$repoRoot/app_server/tmp"
+
+function Copy-WebApp {
+    param (
+        [string]$Src,
+        [string]$Dst
+    )
+
+    Write-Message -Message "Copying app artifacts..." -Color "Green"
+
+    # Ensure the destination directory exists
+    if (-Not (Test-Path -Path $Dst)) {
+        New-Item -ItemType Directory -Path $Dst
+    }
+
+    Copy-Item -Path $Src -Destination $Dst -Recurse
+}
+
+function Cleanup {
+    param (
+        [string]$Tgt
+    )
+
+    Write-Message -Message "Cleaning up target directory: {$Tgt} ..." -Color "Green"
+
+    Remove-Item -Path $Tgt -Recurse -Force
+}
+
+function Deploy-App {
+    Write-Message -Message "Deploying app..."
+
+    Copy-WebApp -Src $webAppBuild -Dst $webAppBuildTmp
+
+    Cleanup -Tgt $webAppBuildTmp
+}
+
+Deploy-App
